bfb50d16d3158844850934842c683a97ac1b384a
Madiyar <seisembaevm@mail.ru>
2020-10-11 03:09:51 +0600

    Madiyar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule,CollapseModule, WavesModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {Page1Component} from './page1/page1.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CarouselComponent} from './page1/Carousel/carousel.component';
+import {PopOverComponent} from './page1/PopOver/PopOver.component';
+
 
 
 @NgModule({
@@ -15,11 +20,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HeaderComponent,
     FooterComponent,
     Page1Component,
+    CarouselComponent,
+    PopOverComponent
+
+
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule,
+    CarouselModule,
+    CollapseModule,
+    WavesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
